82dee030e50ecf01b5dc500da59fd20f4657bc23
baeksnip <76125829+baeksnip@users.noreply.github.com>
2025-03-02 15:17:15 +0100

    Add files via upload

---
+++ argos.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+function ctrl_c(){
+ exit 1
+}
+trap ctrl_c INT
+
+# Config parametros #
+rango="192.168.100"
+ruta_ficheros_puertos="/var/www/html/argos/_puertos/"
+ruta_ficheros_mac="/var/www/html/argos/_mac/"
+ruta_ficheros_macNombres="/var/www/html/argos/_macNombres/"
+ruta_macVendors="/var/www/html/argos/macVendors.txt"
+ruta_ficheros_fecha="/var/www/html/argos/_fecha/"
+ruta_ficheros_nombre="/var/www/html/argos/_nombre/"
+fecha=`date +"%d/%m/%Y %H:%M"`
+# Config parametros #
+
+#Escaneo de IPs
+for ip in $(seq 1 254); do
+        timeout 1 bash -c "ping -c 1 $rango.$ip" &>/dev/null && touch $ruta_ficheros_puertos$rango"."$ip && echo -n $fecha > $ruta_ficheros_fecha$rango"."$ip &
+done; wait
+
+#Consulta MAC si el fichero de la MAC esta vacio
+for fichero_ip in `ls $ruta_ficheros_puertos`; do
+        if ! [ -s $ruta_ficheros_mac$fichero_ip ]; then
+                bash -c "ip neigh | grep $fichero_ip' '" | awk '{print $5}' > $ruta_ficheros_mac$fichero_ip
+        fi
+done
+
+#Consulta MACs para buscar el fabricante
+for fichero_ip in `ls $ruta_ficheros_mac`; do
+	if [ ! -f $ruta_ficheros_macNombres$fichero_ip ]; then
+		touch $ruta_ficheros_macNombres$fichero_ip
+	fi
+	mac_sin_separadores=$(cat "$ruta_ficheros_mac$fichero_ip" | tr -d ':')
+	mac_primeros_seis=$(echo $mac_sin_separadores | grep -Po "^.{6}")
+	empresa=$(grep -i "^$mac_primeros_seis" "$ruta_macVendors" | cut -d'|' -f2)
+	if [ ! -z "$empresa" ]; then
+		echo "$empresa" >> "$ruta_ficheros_macNombres$fichero_ip"
+	fi
+done
+
+#Escaneo de puertos si el fichero de la ip esta vacio 65535
+for fichero_ip in `ls $ruta_ficheros_puertos`; do
+	size=$(wc -c < "$ruta_ficheros_puertos$fichero_ip")
+	if [ "$size" -le 2 ]; then
+		rm $ruta_ficheros_puertos$fichero_ip
+		touch $ruta_ficheros_puertos$fichero_ip
+                for port in $(seq 1 10000); do
+                        timeout 0.1 bash -c "echo '' > /dev/tcp/$fichero_ip/$port" 2>/dev/null && echo -n $port"," >> $ruta_ficheros_puertos$fichero_ip
+                done; wait
+        fi
+done
---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+function check($ip,$puerto) {
+        if ($puerto) {
+                $fp = @fsockopen($ip, $puerto, $errno, $errstr, 1);
+                if ($fp) {
+                        print "<td bgcolor=green width=20></td>";
+                        fclose($fp);
+                }
+                else {
+                        print "<td bgcolor=red width=20></td>";
+                }
+        }
+        else {
+                print "<td bgcolor=red width=20>";
+        }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$ficheroEditado=$_REQUEST['fichero'];
+	$nombreEditado=$_REQUEST['nombre'];
+	$fp = fopen($ficheroEditado, 'w');
+	fwrite($fp, $nombreEditado);
+	fclose($fp);
+}
+
+$fileList = glob('./_puertos/*');
+natsort($fileList);
+
+print("<html><head><title>Argos</title></head>");
+print("<style> table, th, td { border: 1px solid black; border-collapse: collapse; } </style>");
+print ("<table>");
+print ("<tr> <td><b>Fecha</td> <td><b>Activo</td> <td><b>Nombre</td> <td><b>IP</td> <td><b>Puertos</td> <td><b>Mac</td> <td><b>Fabricante</td> </tr>");
+
+foreach($fileList as $filename){
+        if(is_file($filename)){
+                $ip_array = explode ("./_puertos/", $filename);
+                $ip = $ip_array[1];
+                print ("<tr>");
+
+                $ruta_last = "./_fecha/".$ip;
+                $last = fopen($ruta_last,"r");
+                if ($last) {
+                        print ("<td valign='top'>".fgets($last)."</td>");
+                        fclose($last);
+                }
+                $linesP = fopen($filename,'r');
+
+                $puertos = (fgets($linesP));
+                $puerto = explode (",",$puertos);
+                $puerto = ($puerto[0]);
+                fclose($linesP);
+                check($ip,$puerto);
+
+                $ruta_nombre = "./_nombre/".$ip;
+                touch($ruta_nombre);
+                $nombre = fopen($ruta_nombre,"r");
+		print ("<td style='font-size:0px;'><form method='post'><input type='hidden' name='fichero' value='".$ruta_nombre."'><input style='font-size:14px;border:0;outline:0;' type='text' name='nombre' value='".fgets($nombre)."'></form></td>");
+                fclose($nombre);
+
+                print ("<td valign='top'>".$ip."</td>");
+
+                $puertos = fopen($filename,'r');
+		print("<td valign='top'>".fgets($puertos)."</td>");
+                fclose($puertos);
+
+                $ruta_mac = "./_mac/".$ip;
+                $mac = fopen($ruta_mac,"r");
+                print ("<td valign='top'>".fgets($mac)."</td>");
+                fclose($mac);
+
+                $ruta_macNombre = "./_macNombres/".$ip;
+                $macNombre = fopen($ruta_macNombre,"r");
+                print ("<td valign='top'>".fgets($macNombre)."</td>");
+                fclose($macNombre);
+
+                print ("</tr>");
+        }
+}
+print ("</table>");
+print("</html>");
+?>
